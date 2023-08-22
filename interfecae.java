94a3d147e89062156371652f5300b1f9146735cf
UJWALmca <124799607+UJWALmca@users.noreply.github.com>
2023-08-22 07:17:22 +0530

    Add files via upload

---
+++ interfecae.java
@@ -0,0 +1,19 @@
+package java_programs;
+
+public interface email {
+	public void fun(){
+		// TODO Auto-generated method stub
+    System.out.println("fun function");
+    int n= 5 v  ;
+    if(n>2 && n<=5) {
+    	
+    System.out.println("true");
+    }
+    else {
+    	   System.out.println("flase");	
+    }
+	}
+	
+
+}
+   
