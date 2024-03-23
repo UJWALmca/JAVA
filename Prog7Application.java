e7d7b079ad3c80497fc684bd32d0d954ea6777fe
UJWAL <124799607+UJWALmca@users.noreply.github.com>
2024-03-23 23:53:56 +0530

    Add files via upload

---
+++ Prog7Application.java
@@ -0,0 +1,13 @@
+package com.example.prog7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prog7Application.class, args);
+	}
+
+}
