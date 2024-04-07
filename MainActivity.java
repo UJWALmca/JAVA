df2cd9cc063595d51e5cacfa4d706073fb6914f3
UJWAL <124799607+UJWALmca@users.noreply.github.com>
2024-04-07 23:41:52 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,42 +1,91 @@
-package com.example.labcycle04;
+package com.example.mapp2;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    FragmentOne fragmentOne = new FragmentOne();
-    FragmentTwo fragmentTwo = new FragmentTwo();
-    int showingFragments = 0;
+     Button buttonHappy;
+
+    Button buttonangry;
+    Button buttonsad;
+
+    Button buttonlovely;
+        Button buttoncrazy;
+    Button buttonfunny;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.clayout,fragmentOne);
-        showingFragments = 1;
-        fragmentTransaction.commit();
+        buttonHappy = findViewById(R.id.buttonHappy);
+        buttonangry=findViewById(R.id.buttonangry);
+        buttonfunny = findViewById(R.id.buttonfunny);
+      buttonsad = findViewById(R.id.buttonsad);
+        buttoncrazy = findViewById(R.id.buttoncrazy);
+       buttonlovely = findViewById(R.id.buttonlovely);
+//
+//
+//
+        buttonlovely.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToGifActivity("lovely");
+            }
+        });
+
+        buttoncrazy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToGifActivity("crazy");
+            }
+        });
+
+        buttonfunny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToGifActivity("funny");
+            }
+        });
+//
+
+
+        buttonsad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToGifActivity("sad");
+
+            }
+        });
+
+        buttonangry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToGifActivity("angry");
+
+            }
+        });
+
+        buttonHappy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the GifActivity passing the mood
+                navigateToGifActivity("happy");
+            }
+        });
+
+
     }
 
-    public void SwitchFragments(View view){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        if(showingFragments==1){
-            fragmentTransaction.replace(R.id.clayout,fragmentTwo);
-            showingFragments = 2;
-        }
-        else {
-            fragmentTransaction.replace(R.id.clayout,fragmentOne);
-            showingFragments = 1;
-        }
-        fragmentTransaction.commit();
+    private void navigateToGifActivity(String mood) {
+        Intent intent = new Intent(this, GifActivity.class);
+        intent.putExtra("mood", mood);
+        startActivity(intent);
     }
 }
