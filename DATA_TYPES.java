4e64f07b5743ca54a2f7b90a3861bcb5872a6680
UJWAL <124799607+UJWALmca@users.noreply.github.com>
2024-04-16 22:07:01 +0530

    Create DATA_TYPES.java

---
+++ DATA_TYPES.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        byte byteVar = 10;
+        short shortVar = 20;
+        int intVar = 30;
+        long longVar = 40L;
+        float floatVar = 50.5f;
+        double doubleVar = 60.6;
+        char charVar = 'A';
+        boolean booleanVar = true;
+        
+        
+        System.out.println("byteVar: " + byteVar);
+        System.out.println("shortVar: " + shortVar);
+        System.out.println("intVar: " + intVar);
+        System.out.println("longVar: " + longVar);
+        System.out.println("floatVar: " + floatVar);
+        System.out.println("doubleVar: " + doubleVar);
+        System.out.println("charVar: " + charVar);
+        System.out.println("booleanVar: " + booleanVar);
+    }
+}
